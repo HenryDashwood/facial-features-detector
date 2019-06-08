846af70da6b9cfb63a0d161dc1e5bf7b64656362
Henry Dashwood <hcndashwood@gmail.com>
2019-06-08 00:07:46 -0400

    got imgaug generator to work, refactoreed preprocessing

--- code/preprocess.py
+++ code/preprocess.py
@@ -2,44 +2,30 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from PIL import Image
-from tqdm import tqdm
+from fastprogress import progress_bar
 from imgaug import augmenters as iaa
 from imgaug.augmentables.kps import Keypoint, KeypointsOnImage
+from os import listdir
+from matplotlib import image
 
 def load_coordinates_to_dataframe(labels_path):
-
     df = pd.read_csv(labels_path)
-
+    
+    df.columns.values[-1] = "filename"
+    df['filename'] = df['filename'].str.replace('images/', '')
+    df = df.set_index('filename')
+    
+    df = df.drop(['box/_top', 'box/_left', 'box/_width', 'box/_height'], axis=1)
     for col in df:
         if (col[-4:] == 'name'):
-            df = df.drop([col], axis=1)
-    df = df.drop(['box/_top'], axis=1)
-    df = df.drop(['box/_left'], axis=1)
-    df = df.drop(['box/_width'], axis=1)
-    df = df.drop(['box/_height'], axis=1)
-
-    df.columns.values[-1] = "filename"
-    for i in range(df.shape[0]):
-        new = {df['filename'].values[i].replace('images/', '') for x in df['filename'].values[i]}
-        new = repr(new)
-        new = new[2:]
-        new = new[:-2]
-        df['filename'].values[i] = new
-
-    i = 0
-    j = 0
-    while (i < 22):
-        df.columns.values[i] = (str(j) + "_x")
-        df.columns.values[i+1] = (str(j) + "_y")
-        i += 2
-        j += 1
-
-    df = df.set_index('filename')
+            df = df.drop([col], axis=1) 
+    
+    df.columns = df.columns.str.replace('box/part/', '')
+    df.columns = df.columns.str.replace('/', '')
 
     return df
 
-def resize(image_name, img, df):
-    
+def resize(image_name, img, df):  
     start_coords = np.array(list(df.loc[image_name])).reshape((11,2))
 
     keypoints = KeypointsOnImage.from_xy_array(start_coords, shape=img.shape)
@@ -48,10 +34,7 @@ def resize(image_name, img, df):
         iaa.Resize({"height": 80, "width": 80})
     ])
 
-    seq_det = seq.to_deterministic()
-
-    image_aug = seq_det.augment_images([img])[0]
-    keypoints_aug = seq_det.augment_keypoints([keypoints])[0]
+    image_aug, keypoints_aug = seq(image=img, keypoints=keypoints)
         
     coords = keypoints_aug.to_xy_array().flatten()
 
@@ -60,11 +43,10 @@ def resize(image_name, img, df):
     return image_aug, df
 
 def load(images_path, labels_path):
-
     y = load_coordinates_to_dataframe(labels_path)
     X = []
-
-    for filename in tqdm(list(y.index)):
+                
+    for filename in progress_bar(list(y.index)):
         img = np.array(Image.open(images_path+filename))
         if len(img.shape) == 3 and img.shape[2] == 3:
             img, y = resize(filename, img, y)
