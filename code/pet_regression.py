bc21327f875b6224be5aa4dec6207963954301ef
EC2 Default User <ec2-user@ip-172-16-182-149.ec2.internal>
2020-01-15 00:07:21 +0000

    lkjdsfsdk|

--- code/pet_regression.py
+++ code/pet_regression.py
@@ -2,8 +2,6 @@ from bentoml import BentoService, api, env, artifacts
 from bentoml.artifact import FastaiModelArtifact
 from bentoml.handlers import FastaiImageHandler
 
-# from fastai.vision import *
-
 @env(pip_dependencies=['gevent', 'pillow==6.2.2', 'fastai'])
 @artifacts([FastaiModelArtifact('pet_regressor')])
 class PetRegression(BentoService):
@@ -11,4 +9,4 @@ class PetRegression(BentoService):
     @api(FastaiImageHandler)
     def predict(self, image):
         result = self.artifacts.pet_regressor.predict(image)
-        return result
+        return [result[0], result[1].tolist(), result[2].tolist()]
